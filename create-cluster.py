51771b354ba835eb2d61e6708f2cc4dfcbaf19c6
Oleksandr Shulgin <oleksandr.shulgin@zalando.de>
2016-02-15 13:44:40 +0100

    Restructure launch code, clean up on errors.

--- create-cluster.py
+++ create-cluster.py
@@ -7,7 +7,7 @@ import yaml
 from clickclick import Action, info
 
 
-def setup_security_groups(cluster_name: str, public_ips: dict) -> dict:
+def setup_security_groups(cluster_name: str, public_ips: dict, result: dict) -> dict:
     '''
     Allow traffic between regions
 
@@ -22,15 +22,16 @@ def setup_security_groups(cluster_name: str, public_ips: dict) -> dict:
             sg_name = cluster_name
             sg = ec2.create_security_group(GroupName=sg_name, VpcId=vpc_id,
                     Description='Allow cassandra nodes to talk via port 7001')
+            result[region] = sg
 
             ec2.create_tags(Resources=[sg['GroupId']],
                             Tags=[{'Key': 'Name', 'Value': sg_name}])
             ip_permissions = []
             for ip in ips:
                 ip_permissions.append({'IpProtocol': 'tcp',
-                                       'FromPort': -1,
+                                       'FromPort': 7001, # port range: From-To
                                        'ToPort': 7001,
-                                       'IpRanges': [{'CidrIp': '{}/32'.format(ip)}]})
+                                       'IpRanges': [{'CidrIp': '{}/32'.format(ip['PublicIp'])}]})
             ip_permissions.append({'IpProtocol': '-1',
                                    'UserIdGroupPairs': [{'GroupId': sg['GroupId']}]})
             ec2.authorize_security_group_ingress(GroupId=sg['GroupId'],
@@ -80,6 +81,34 @@ def generate_taupage_user_data(cluster_name: str, seed_nodes: list):
     return user_data
 
 
+def allocate_public_ips(regions: list, cluster_size: int, public_ips: dict):
+    # reservice Elastic IPs
+    for region in regions:
+        with Action('Allocating Public IPs for {}..'.format(region)) as act:
+            ec2 = boto3.client('ec2', region_name=region)
+            for i in range(cluster_size):
+                resp = ec2.allocate_address(Domain='vpc')
+                public_ips[region].append(resp)
+                act.progress()
+
+
+def launch_instance(region: str, ip: str, instance_type: str, ami: str, user_data: str,
+                    security_group_id: str):
+
+    with Action('Launching node {}..'.format(ip)):
+        ec2 = boto3.client('ec2', region_name=region)
+
+        resp = ec2.describe_subnets()
+        # subnet IDs sorted by AZ
+        subnets = list([subnet['SubnetId'] for subnet in sorted(resp['Subnets'], key=lambda subnet: subnet['AvailabilityZone'])])
+
+        # start seed node in first AZ
+        ec2.run_instances(ImageId=ami.id, MinCount=1, MaxCount=1,
+                SecurityGroupIds=[security_group_id],
+                UserData=user_data, InstanceType=instance_type,
+                SubnetId=subnets[0])
+
+
 @click.command()
 @click.option('--cluster-size', default=3, type=int)
 @click.option('--instance-type', default='t2.micro')
@@ -92,51 +121,46 @@ def cli(cluster_name: str, regions: list, cluster_size: int, instance_type: str)
     # generate keystore/truststore
 
     # Elastic IPs by region
-    # Let's assume the first IP in each region is the seed node
     public_ips = collections.defaultdict(list)
+    security_groups = {}
+    try:
+        allocate_public_ips(regions, cluster_size, public_ips)
 
-    # reservice Elastic IPs
-    for region in regions:
-        with Action('Allocating Public IPs for {}..'.format(region)) as act:
-            ec2 = boto3.client('ec2', region_name=region)
-            for i in range(cluster_size):
-                resp = ec2.allocate_address(Domain='vpc')
-                public_ips[region].append(resp['PublicIp'])
-                act.progress()
-
-    # Now we have all necessary Public IPs
-    # take first IP in every region as seed node
-    seed_nodes = []
-    for region in regions:
+        # take first IP in every region as seed node
         # TODO: support more than one seed node per region for larger clusters
-        seed_nodes.append(public_ips[region][0])
-    info('Our seed nodes are: {}'.format(', '.join(seed_nodes)))
+        seed_nodes = { region: ips[0]['PublicIp'] for region, ips in public_ips.items() }
+        info('Our seed nodes are: {}'.format(', '.join(seed_nodes.values())))
 
-    # Set up Security Groups
-    security_groups = setup_security_groups(cluster_name, public_ips)
+        # Set up Security Groups
+        setup_security_groups(cluster_name, public_ips, security_groups)
 
-    taupage_amis = find_taupage_amis(regions)
-    user_data = generate_taupage_user_data(cluster_name, seed_nodes)
+        taupage_amis = find_taupage_amis(regions)
+        user_data = generate_taupage_user_data(cluster_name, seed_nodes)
 
-    # Launch EC2 instances with correct user data
-    # Launch sequence:
-    # start seed nodes (e.g. 1 per region if cluster_size == 3)
-    for ip, region in zip(seed_nodes, regions):
-        with Action('Launching seed node {}..'.format(ip)):
-            ec2 = boto3.client('ec2', region_name=region)
-            resp = ec2.describe_subnets()
-            # subnet IDs sorted by AZ
-            subnets = []
-            for subnet in sorted(resp['Subnets'], key=lambda subnet: subnet['AvailabilityZone']):
-                subnet.append(subnet['SubnetId'])
-            # start seed node in first AZ
-            ec2.run_instances(ImageId=taupage_amis[region], MinCount=1, MaxCount=1, SecurityGroupIds=[security_groups[region]],
-                    UserData=user_data, InstanceType=instance_type,
-                    SubnetId=subnets[0])
+        # Launch EC2 instances with correct user data
+        # Launch sequence:
+        # start seed nodes (e.g. 1 per region if cluster_size == 3)
+        for region, ip in seed_nodes.items():
+            launch_instance(region, ip, instance_type=instance_type,
+                            ami=taupage_amis[region], user_data=user_data,
+                            security_group_id=security_groups[region]['GroupId'])
 
+        # make sure all seed nodes are up
+        # add remaining nodes one by one
 
-    # make sure all seed nodes are up
-    # add remaining nodes one by one
+    except:
+        for region, sg in security_groups.items():
+            ec2 = boto3.client('ec2', region)
+            info('Cleaning up security group: {}'.format(sg['GroupId']))
+            ec2.delete_security_group(GroupId=sg['GroupId'])
 
+        for region, ips in public_ips.items():
+            ec2 = boto3.client('ec2', region)
+            for ip in ips:
+                info('Releasing IP address: {}'.format(ip['PublicIp']))
+                ec2.release_address(AllocationId=ip['AllocationId'])
+
+        raise
+    
 if __name__ == '__main__':
     cli()
