954bcc539066136008cba03069bb195e408735ba
Henning Jacobs <henning@jacobs1.de>
2016-02-15 12:11:21 +0100

    allocate address

--- create-cluster.py
+++ create-cluster.py
@@ -2,6 +2,9 @@
 
 import boto3
 import click
+import collections
+from clickclick import Action
+
 
 @click.command()
 @click.option('--cluster-size', default=3, type=int)
@@ -11,10 +14,20 @@ def cli(regions, cluster_size):
         raise click.UsageError('Please specify at least one region')
 
     # generate keystore/truststore
+
+    # Elastic IPs by region
+    public_ips = collections.defaultdict(list)
+
     # reservice Elastic IPs
     for region in regions:
-        ec2 = boto3.client('ec2', region_name=region)
-        resp = ec2.allocate_address(DryRun=True, Domain='vpc')
+        with Action('Allocating Public IPs for {}..'.format(region)) as act:
+            ec2 = boto3.client('ec2', region_name=region)
+            for i in range(cluster_size):
+                resp = ec2.allocate_address(Domain='vpc')
+                public_ips[region].append(resp['PublicIp'])
+                act.progress()
+
+    # Now we have all necessary Public IPs
 
     # Launch EC2 instances with correct user data
 
